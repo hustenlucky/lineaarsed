47d00d8900cd81c58caf2995be93308a583328c6
Anna Oleks <annaoleks88@gmail.com>
2025-02-25 15:54:54 +0200

    я сделал задание 1.2.3.4 задание научились работать и находить pi

---
+++ tund 1.py
@@ -0,0 +1,36 @@
+# # from random import * # importeerime kõok funktisoonid, mis on random moodulis
+# from math import *
+# # # # nimi=input("Mis on sinu nimi?: ")
+# # # # vanus=int(input("kui vana sa oled?: "))
+
+# # # # print(f"Tere, maailm! Tervitan sind {nimi} Sa oled {vanus} aastat vana.")
+# # # # print("Tere, maailm! Tervitan sind",nimi,"Sa oled",vanus,"aastat vana.")
+# # # # print("Tere, maailm! Tervitan sind"+nimi+"\nSa oled"+str(vanus)+"aastat vana.")
+# # # #Ülesanne 2
+# # #  # from re import Match
+
+
+# # # # vanus = 18
+# # # #  eesnimi = "Jaak"
+# # # #  pikkus = 16.5
+# # # #  kas_käib_koolis = True
+# # # # print(f"Muutuja {vanus} on {type(vanus)} tüübi")
+# # # # print(f"Muutuja {eesnimi} on {type(eesnimi)} tüübi")
+# # # # print(f"Muutuja {pikkus} on {type(pikkus)} tüübi")
+# # # # print(f"Muutuja {kas_käib_koolis} on {type(kas_käib_koolis)} tüübi")
+# # # #Ulesanne 3
+# # # kommidearv=randint(1,99)
+# # # print(f"Laual on {kommidearv} komme")
+# # # kommidvõtmud=int(input("Mitu kommi tahad ära võtta?"))
+# # # onjäänud=kommidearv-kommidvõtmud
+# # # print(f"Laual on jäänud {onjäänud} komme")
+# # #Ulesanne 4
+# from math import pi
+
+
+# ümbermõõt=int(input("kui suur on ümbermõtt?: "))
+# läbimõõt=ümbermõõt//pi
+# print(f"läbimõõt on +f{läbimõõt}")
+#Ülesanne 5
+
+
